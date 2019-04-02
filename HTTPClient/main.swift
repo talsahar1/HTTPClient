a04625b82229d28eee1e62ec6ca637b0edad23c9
“TalGlobalbit” <tal@globalbit.co.il>
2019-04-02 23:11:44 +0300

    Initial Commit

---
+++ HTTPClient/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HTTPClient
+//
+//  Created by Tal Sahar on 4/2/19.
+//  Copyright © 2019 Tal Sahar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
